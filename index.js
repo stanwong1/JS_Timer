70c14347fbf0a9e9f34cde53cd1e96651b7d7bce
Stan <mr.stanwong@gmail.comm>
2020-04-08 21:38:17 -0700

    Javascript Timer

---
+++ index.js
@@ -0,0 +1,26 @@
+const durationInput = document.querySelector("#duration");
+const startButton = document.querySelector("#start");
+const pauseButton = document.querySelector("#pause");
+const circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+        duration = totalDuration;
+        console.log('Timer has intiated')
+    },
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset',
+        perimeter * timeRemaining / duration - perimeter
+        );
+        console.log('ticking');
+        
+    },
+    onComplete() {
+        console.log('Timer is completed');
+    }
+});
---
+++ timer.js
@@ -0,0 +1,53 @@
+class Timer {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
+        this.durationInput = durationInput;
+        this.startButton = startButton;
+        this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
+
+        this.timeLeft = 30;
+
+        this.startButton.addEventListener('click', this.start);
+        this.pauseButton.addEventListener('click', this.pause);
+    }
+
+    start = () => {
+        if (this.onStart) {
+            this.onStart(this.timeRemaining);
+        }
+        this.tick();
+        this.interval = setInterval(this.tick, 50);
+    };
+
+    pause = () => {
+        clearInterval(this.interval);
+    };
+
+    tick = () => {
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            //match back with interval timer the .05
+            this.timeRemaining = this.timeRemaining - .05;
+            if (this.onTick) {
+                this.onTick(this.timeRemaining);
+            }
+        }
+    };
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        //toFixed - allows only 2 decimals
+        this.durationInput.value = time.toFixed(2);
+    }
+}
